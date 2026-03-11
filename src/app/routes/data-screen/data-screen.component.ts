49e08fc8310c23f188d14e3585ff15c208960487
tom-cfxu <cfxu963852741@qq.com>
2026-03-11 23:38:34 +0800

    feat:新增组件

--- src/app/routes/data-screen/data-screen.component.ts
+++ src/app/routes/data-screen/data-screen.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DataTitleComponent } from './widgets/title/title.component';
+import { Box1Component } from './widgets/box1/box1.component';
 
 @Component({
   selector: 'app-data-screen',
-  imports: [ DataTitleComponent ],
+  imports: [ DataTitleComponent,Box1Component ],
   templateUrl: './data-screen.component.html',
   styleUrl: './data-screen.component.less'
 })
