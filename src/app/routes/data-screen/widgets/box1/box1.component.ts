382ea75bfb8f84455c350b2a9c0f1e73e1adb949
tom-cfxu <cfxu963852741@qq.com>
2026-03-11 22:54:01 +0800

    fix:修改样式

---
+++ src/app/routes/data-screen/widgets/box1/box1.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box1',
+  imports: [],
+  templateUrl: './box1.component.html',
+  styleUrl: './box1.component.less'
+})
+export class Box1Component {}
--- src/app/routes/data-screen/widgets/title/title.component.ts
+++ src/app/routes/data-screen/widgets/title/title.component.ts
@@ -2,7 +2,7 @@
  * @Author: tom-cfxu cfxu963852741@qq.com
  * @Date: 2026-03-11 19:00:37
  * @LastEditors: tom-cfxu cfxu963852741@qq.com
- * @LastEditTime: 2026-03-11 22:50:43
+ * @LastEditTime: 2026-03-11 22:51:33
  * @FilePath: \my-project\src\app\layout\blank\widgets\time.component.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
