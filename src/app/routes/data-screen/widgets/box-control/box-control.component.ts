43300a976781131324a245f99d9e84bb74d92373
tom-cfxu <cfxu963852741@qq.com>
2026-03-13 02:07:40 +0800

    feat:新增弹窗事件

--- src/app/routes/data-screen/widgets/box-control/box-control.component.ts
+++ src/app/routes/data-screen/widgets/box-control/box-control.component.ts
@@ -1,24 +1,96 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-
+import { CommonModule } from '@angular/common';
+import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
+import { EvaluationResultsComponent } from '../evaluation-results/evaluation-results.component';
 @Component({
   selector: 'app-box-control',
-  imports: [NzButtonModule],
+  imports: [NzButtonModule, NzModalModule, CommonModule],
   styleUrls: ['./box-control.component.less'],
   template: `
     <div class="container">
-        <div class="control">当前用能管控模式:<h1>手动</h1> </div>
-        <div class="control">当前用能管控模式:<h1>低客流 <a class="b1" nz-button nzType="link">切换</a></h1>  </div>
-        <div class="my-button">
-        <img src="../../../../../assets/tmp/home/logo5.png">
-        切换节能模式</div>
-        <div class="my-button">
-        <img src="../../../../../assets/tmp/home/logo5.png">
-        车站碳排放评估</div>
+      <div class="control"
+        >当前用能管控模式:<h1 [ngStyle]="{ color: modeObject[mode]['color'] }">{{ modeObject[mode]['title'] }}</h1>
       </div>
-      <div class="button2">
-        <button nz-button nzType="primary">重置</button>
+      <div class="control"
+        >当前用能管控模式:<h1>
+          <span [ngStyle]="{ color: levelObject[level]['color'] }">{{levelObject[level]['title']}}</span> <a class="b1" nz-button nzType="link" (click)="levelChange()">切换</a></h1
+        >
       </div>
-  `,
+      <div class="my-button" (click)="changeMode()" [ngStyle]="{ color: modeObject[mode]['reverseColor'],'border-color':modeObject[mode]['reverseColor'] }">
+        <img src="../../../../../assets/tmp/home/logo5.png" />
+        切换{{ modeObject[mode]['reverse'] }}模式</div
+      >
+      <div class="my-button" (click)="evaluate()">
+        <img src="../../../../../assets/tmp/home/logo5.png" />
+        车站碳排放评估</div
+      >
+    </div>
+    <div class="button2">
+      <button nz-button nzType="primary">重置</button>
+    </div>
+  `
 })
-export class BoxCeterControlComponent {}
+export class BoxCeterControlComponent {
+  constructor(private modal: NzModalService) {}
+  private readonly cdr = inject(ChangeDetectorRef);
+  mode: number = 1;
+  level: number = 0;
+
+  modeObject: any = {
+    1: {
+      title: '手动',
+      color: '#5087ec',
+      reverse: '节能',
+      reverseColor:'#58a55c'
+    },
+    0: {
+      title: '节能',
+      color: '#58a55c',
+      reverse: '手动',
+      reverseColor:'#5087ec'
+    }
+  };
+  levelObject: any = {
+    1: {
+      title: '高客流',
+      color: '#5087ec'
+    },
+    0: {
+      title: '低客流',
+      color: '#f57931'
+    }
+  };
+
+  evaluate() {
+    this.modal.create({
+      nzContent: EvaluationResultsComponent,
+      nzClassName: 'empty-modal',
+      nzMaskClosable: true,
+      nzWidth: 800,
+      nzData: {
+        result: 2
+      }
+    });
+  }
+
+  levelChange(){
+    this.level = Number(!Boolean(this.level));
+    this.cdr.detectChanges();
+  }
+  changeMode(): void {
+    this.modal.create({
+      nzTitle: this.mode == 1 ? '是否切换节能模式？' : '是否切换手动模式？',
+      nzClassName: 'energy-modal-confirm',
+      nzCentered: true,
+      nzMaskClosable: false,
+      nzOnOk: () => {
+        this.mode = Number(!Boolean(this.mode));
+
+        setTimeout(()=>{
+          this.cdr.detectChanges();
+        })
+      }
+    });
+  }
+}
--- src/app/routes/data-screen/widgets/device-manage/device-manage.component.ts
+++ src/app/routes/data-screen/widgets/device-manage/device-manage.component.ts
@@ -2,7 +2,7 @@
  * @Author: tom-cfxu cfxu963852741@qq.com
  * @Date: 2026-03-12 02:06:23
  * @LastEditors: tom-cfxu cfxu963852741@qq.com
- * @LastEditTime: 2026-03-13 00:32:49
+ * @LastEditTime: 2026-03-13 01:40:26
  * @FilePath: \fuzhou-subway-project\src\app\routes\data-screen\widgets\device-manage\device-manage.component.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -31,7 +31,6 @@ export class DeviceManageComponent {
   private readonly cdr = inject(ChangeDetectorRef);
   constructor(
     private modal: NzModalService,
-    private viewContainerRef: ViewContainerRef
   ) {}
 
 device_light:any[]=[
@@ -53,7 +52,7 @@ device_air:any[]=[
 ];
 
 changePower(item:any):void{
-  console.log('item',item);
+  // console.log('item',item);
   const index=item['index'];
   this.modal.create({
     nzTitle:item.title,
