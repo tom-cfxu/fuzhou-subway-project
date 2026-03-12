6ff04b0a14286fe23e795aa7e8997b968ea08e04
tom-cfxu <cfxu963852741@qq.com>
2026-03-13 02:07:53 +0800

    fix:修改bug

--- src/app/routes/data-screen/widgets/evaluation-results/evaluation-results.component.ts
+++ src/app/routes/data-screen/widgets/evaluation-results/evaluation-results.component.ts
@@ -2,7 +2,7 @@
  * @Author: tom-cfxu cfxu963852741@qq.com
  * @Date: 2026-03-13 01:07:11
  * @LastEditors: tom-cfxu cfxu963852741@qq.com
- * @LastEditTime: 2026-03-13 01:41:29
+ * @LastEditTime: 2026-03-13 02:07:34
  * @FilePath: \fuzhou-subway-project\src\app\routes\data-screen\widgets\evaluation-results\evaluation-results.component.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -34,6 +34,7 @@ export class EvaluationResultsComponent implements OnInit {
     }
   }
   readonly modalRef = inject(NzModalRef);
+  
   medalObject:any={
     1:{
       result:'节能大师',
