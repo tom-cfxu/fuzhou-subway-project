01d49172a633a9e63bce7d70787b89a89dd433b1
tom-cfxu <1157164295@qq.com>
2026-03-27 14:38:41 +0800

    修复冲突

--- src/app/layout/blank/widgets/time.component.ts
+++ src/app/layout/blank/widgets/time.component.ts
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-=======
+/* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-explicit-any */
->>>>>>> da712bb (init)
 /*
  * @Author: tom-cfxu cfxu963852741@qq.com
  * @Date: 2026-03-11 19:00:37
@@ -10,7 +8,7 @@
  * @FilePath: \my-project\src\app\layout\blank\widgets\time.component.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { AfterViewInit, ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, inject, OnDestroy } from '@angular/core';
 import moment from 'moment';
 @Component({
   selector: 'header-time',
@@ -39,7 +37,6 @@ export class HeaderTimeComponent implements OnDestroy,AfterViewInit{
       clearInterval(this.timer);
     }
   }
-
 
 
 }
